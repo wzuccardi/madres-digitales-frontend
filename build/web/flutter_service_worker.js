d1eead001028a61c20609706f7e8a203d8e81e23
wzuccardi <wzuccardi@gmail.com>
2025-10-26 00:31:14 -0500

    Update frontend with fixed dashboard service and auth service

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "607892e8b5797fd1c43d3e0fec79b3ad
 "debug-auth.html": "00f9f7b27e006e4ebcaeac0a9860b20a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "e67ce2b1c71433ddb9141cf21a1dcb72",
+"flutter_bootstrap.js": "b4e437c7089de43071083bd720404286",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
