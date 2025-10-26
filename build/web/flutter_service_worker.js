65a9d8b318d20aff0b96221f7ec394f5e47bd867
wzuccardi <wzuccardi@gmail.com>
2025-10-25 20:55:01 -0500

    add_V1.4

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "607892e8b5797fd1c43d3e0fec79b3ad
 "debug-auth.html": "00f9f7b27e006e4ebcaeac0a9860b20a",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "4e19459ceefbf8fb1a6d5c3c41a6d1e6",
+"flutter_bootstrap.js": "ea7815e1236fd0095713610f0734e37e",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
